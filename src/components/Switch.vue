8c47e91fc93c80e5dcb585bac549256cd1ae83c6
Haoliang Wu <littlelyon@aliyun.com>
2018-10-19 16:27:42 +0800

    [ADD] part1 build a toggle comp

---
+++ src/components/Switch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="toggle">
+	  <input class="toggle-input" type="checkbox" />
+	  <button class="toggle-btn" :class="[className, {'toggle-btn-on': on, 'toggle-btn-off': !on}]" :aria-expanded="on"></button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    on: {
+      type: Boolean,
+      default: false
+    },
+    className: String
+  }
+}
+</script>
+<style>
+
+</style>
+
---
+++ src/components/Toggle.vue
@@ -0,0 +1,34 @@
+<template>
+  <switch-comp :on="checked" @click.native="onClick"></switch-comp>
+</template>
+<script>
+import Switch from "./Switch";
+
+export default {
+  props: {
+    on: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data(){
+    return {
+      checked: false
+    }
+  },
+  components: {
+    SwitchComp: Switch
+  },
+  methods: {
+    onClick() {
+      this.checked = !this.checked
+      this.$emit("toggled", this.checked);
+    }
+  },
+  mounted(){
+    this.checked = this.on
+  }
+};
+</script>
+<style>
+</style>
